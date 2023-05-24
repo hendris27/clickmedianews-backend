cabd1f8631109664204a31aaaf97fb05ec86fbe6
DausHamyR <daus.amat354@gmail.com>
2023-05-25 00:52:39 +0700

    article view post data

--- src/routers/users/articleView.router.js
+++ src/routers/users/articleView.router.js
@@ -1,6 +1,6 @@
 const articleViewRouter = require("express").Router();
 const articleViewController = require("../../controllers/auth/users/articleView.controller");
 
-articleViewRouter.get("/", articleViewController.createComment);
+articleViewRouter.post("/", articleViewController.createComment);
 
 module.exports = articleViewRouter;
