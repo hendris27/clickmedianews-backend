95dcfb4f89560ec476fd1bf442a9cc60fc53a56d
putubagus13 <putu031216@gmail.com>
2023-05-25 23:49:36 +0700

    repair typing

--- src/controllers/admin/writeArticle.controller.js
+++ src/controllers/admin/writeArticle.controller.js
@@ -1,29 +1,29 @@
 const articleModel = require("../../models/articles.model");
 const errorHandler = require("../../../helpers/errorHandler.halper");
-const categoryModel = require("../../models/categories.model")
+const categoryModel = require("../../models/categories.model");
 
 exports.createArticle = async (request, response) =>{
     try {
-        const {id} = request.user
+        const {id} = request.user;
         const data = {
             ...request.body,
             createdBy: id,
             status: true
-        }
-        const crtArticle = await articleModel.insert(data)
+        };
+        const crtArticle = await articleModel.insert(data);
         if(!crtArticle){
-            throw Error("Create article failed")
+            throw Error("Create article failed");
         }
         if(request.file){
-            data.picture = request.file.path
+            data.picture = request.file.path;
         }
-        await categoryModel.insert(data.category)
+        await categoryModel.insert(data.category);
         return response.json({
             success: true,
             message: "Write article success",
             results: crtArticle
-        })
+        });
     } catch (err) {
-        return errorHandler(response, err)
+        return errorHandler(response, err);
     }
-}
+};
