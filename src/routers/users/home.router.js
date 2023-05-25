aeb11b9bd11f9caf4dbb2160c381ac55c86530da
DausHamyR <daus.amat354@gmail.com>
2023-05-25 19:50:04 +0700

    delete home

--- src/routers/users/home.router.js
+++
@@ -1,6 +0,0 @@
-const homeRouter = require("express").Router();
-const homeController = require("../controllers/auth/users/home.controller");
-
-homeRouter.get("/", homeController.getHome);
-
-module.exports = homeRouter;
