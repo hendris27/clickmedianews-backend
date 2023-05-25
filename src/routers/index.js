54c1ff8870b4c8b2d4f93186bacccf73ba7257d5
putubagus13 <putu031216@gmail.com>
2023-05-25 12:02:09 +0700

    add: article admin

--- src/routers/index.js
+++ src/routers/index.js
@@ -11,6 +11,7 @@ router.use("/auth/admin", require("./auth/admin/auth.router"));
 router.use("/auth/user", require("./auth/users/auth.router"));
 router.use("/admin", require("./admin/admin.router"));
 router.use("/article/user", require("./users/article.router"));
+router.use("/article/admin", require("./admin/article.router"));
 router.use("/category/user", require("./users/category.router"));
 router.use("/home/user", require("./users/home.router"));
 router.use("/home/admin", require("./admin/home.admin.router"));
