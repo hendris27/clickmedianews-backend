3187e64b3a5d1c3a6b0c8b72418591d03bbc18cd
putubagus13 <putu031216@gmail.com>
2023-05-27 22:04:45 +0700

    fix: article manage

--- src/controllers/article.controller.js
+++ src/controllers/article.controller.js
@@ -3,7 +3,7 @@ const errorHandler = require("../helpers/errorHandler.helper");
 
 exports.getArticle = async function (request, response) {
     try {
-        const { rows: results, pageInfo } = await articleModel.findAllArticle(request.params);
+        const { rows: results, pageInfo } = await articleModel.findAllArticle(request.query);
         return response.json({
             success: true,
             message: "Get save post success",
@@ -14,4 +14,59 @@ exports.getArticle = async function (request, response) {
         return errorHandler(response, error);
     }
 };
+
+exports.getArticleManage = async function (request, response) {
+    try {
+        const {id} = request.user;
+        if(!id){
+            throw Error("article not found");
+        }
+        const { rows: results, pageInfo } = await articleModel.findAllArticleManage(request.query, id);
+        return response.json({
+            success: true,
+            message: "Get save post success",
+            pageInfo,
+            results,
+        });
+    } catch (error) {
+        return errorHandler(response, error);
+    }
+};
+
+exports.getOneArticleManage = async function (request, response) {
+    try {
+        const {id} = request.user;
+        if(!id){
+            throw Error("article not found");
+        }
+        const { rows: results, pageInfo } = await articleModel.findOneArticleManage(request.query, id, request.params.id);
+        return response.json({
+            success: true,
+            message: "Get save post success",
+            pageInfo,
+            results,
+        });
+    } catch (error) {
+        return errorHandler(response, error);
+    }
+};
+
+exports.deleteArticle = async function(request, response){
+    try {
+        const data = await articleModel.findOne(request.params.id);
+        if(!data){
+            throw Error("article not found");
+        }
+        const dltArticle = await articleModel.destroy(request.params.id);
+        return response.json({
+            success: true,
+            message: "Delete article success",
+            results: dltArticle
+        });
+    } catch (error) {
+        return errorHandler(response,error);
+    }
+};
+
+
 
