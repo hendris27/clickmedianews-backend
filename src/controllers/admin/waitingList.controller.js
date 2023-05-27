b73db2529644c0fbf0c412c39a01c1f31869a753
putubagus13 <putu031216@gmail.com>
2023-05-27 11:35:14 +0700

    fix: waiting list update and delete

--- src/controllers/admin/waitingList.controller.js
+++ src/controllers/admin/waitingList.controller.js
@@ -16,25 +16,56 @@ exports.getArticle = async (request, response) => {
 
 exports.updateArticle = async function (request, response) {
     try {
-        const article = await articleModel.findOne(request.body);
+        const {articleId} = request.body;
+        const article = await articleModel.findOne(articleId);
+        console.log(article);
         if (!article) {
             throw Error("Article not found");
         }
         if (!article.status) {
             const data = {
-                ...request.body,
+                ...request.body
             };
             if (request.file) {
                 data.picture = request.file.path;
             }
-            const accPublished = await articleModel.update(article.id);
-            return response.json({
-                success: true,
-                message: "Article published success",
-                results: accPublished,
-            });
+            const accPublished = await articleModel.update(article.id, data);
+            console.log(accPublished);
+            if(accPublished){
+                return response.json({
+                    success: true,
+                    message: "Article published success",
+                    results: accPublished,
+                });
+            }else{
+                throw Error("published failed");
+            }
+            
         }
     } catch (err) {
         return errorHandler(response, err);
+    }
+};
+
+exports.ignoreArticle = async function(request, response){
+    try {
+        const {articleId} = request.body;
+        const data = await articleModel.findOne(articleId);
+        if(!data){
+            throw Error("article not found");
+        }
+        const deleteArticle = await articleModel.destroy(articleId);
+        if(deleteArticle){
+            return response.json({
+                success: true,
+                message: "Delete aticle success",
+                results: deleteArticle
+            });
+        }else{
+            throw Error("Ignore article failed");
+        }
+        
+    } catch (error) {
+        return errorHandler(response,error);
     }
 };
