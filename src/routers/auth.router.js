206400078a30092a9b58b420e6d26561ffc3788c
putubagus13 <putu031216@gmail.com>
2023-05-28 00:02:45 +0700

    fix: router.js

--- src/routers/auth.router.js
+++ src/routers/auth.router.js
@@ -1,6 +1,6 @@
 const authRouter = require("express").Router();
 const authController = require("../controllers/auth.controller");
-const validate = require("../middlewares/auth.middleware");
+const validate = require("../middlewares/validator.middleware");
 
 authRouter.post("/login", validate("authLogin"), authController.login);
 authRouter.post("/register", validate("authRegister"), authController.register);
