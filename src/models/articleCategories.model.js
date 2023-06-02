b380531d98615564875d707b8a88e02dc7846e06
hendri27 <88545100+hendris27@users.noreply.github.com>
2023-06-02 17:27:36 +0700

    add nodel categories

--- src/models/articleCategories.model.js
+++ src/models/articleCategories.model.js
@@ -18,3 +18,28 @@ exports.destroy = async function (id) {
     const {rows} = await db.query(query, values);
     return rows[0];
 };
+
+exports.findAllArticleCategory = async function (params) {
+    params.page = parseInt(params.page) || 1;
+    params.limit = parseInt(params.limit) || 10;
+    params.search = params.search || "";
+    params.sort = params.sort || "id";
+    params.sortBy = params.sortBy || "ASC";
+
+    const offset = (params.page - 1) * params.limit;
+
+    const query = `
+    SELECT
+    "c"."id",
+    "c"."name",
+    "a"."picture"
+    FROM "categories" "c"
+    JOIN "articles" "a" ON "a"."categoryId" = "c"."id"
+    WHERE "c"."name" :: TEXT LIKE $3
+    ORDER BY ${params.sort} ${params.sortBy}
+    LIMIT $1 OFFSET $2
+    `;
+    const values = [params.limit, offset, `%${params.search}%`];
+    const { rows } = await db.query(query, values);
+    return rows;
+};
